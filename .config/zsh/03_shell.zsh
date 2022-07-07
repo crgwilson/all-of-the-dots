4a59b6f2cae0d83d225ceb28d95a46a676578abf
Craig Wilson <craigjwilson93@gmail.com>
2022-07-06 20:40:48 -0700

    Case insensitive zsh completion

--- .config/zsh/03_shell.zsh
+++ .config/zsh/03_shell.zsh
@@ -2,6 +2,7 @@ zstyle ':completion:*' use-cache on
 zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/zsh_completion_cache"
 zstyle ':completion:*' menu select
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 
 # vi mode
 bindkey -v
