9037ecfe7b8bb60229cf610479f67fdc5f010c80
Craig Wilson <craigjwilson93@gmail.com>
2022-07-07 17:46:10 -0700

    Add fzf & git-number

--- .config/zsh/99_fzf.zsh
+++ .config/zsh/99_fzf.zsh
@@ -1,1 +1,9 @@
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+if [ -d "$XDG_DATA_HOME/fzf" ]; then
+  # Auto-completion
+  # ---------------
+  [[ $- == *i* ]] && source "$XDG_DATA_HOME/fzf/shell/completion.zsh" 2> /dev/null
+
+  # Key bindings
+  # ------------
+  source "$XDG_DATA_HOME/fzf/shell/key-bindings.zsh"
+fi
