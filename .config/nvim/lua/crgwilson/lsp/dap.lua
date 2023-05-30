d0ad17da4983326816837faf6fd06e08f2b63812
Craig Wilson <craigjwilson93@gmail.com>
2023-05-29 17:09:43 -0700

    Make dap-ui console the largest window on the bottom while debugging

--- .config/nvim/lua/crgwilson/lsp/dap.lua
+++ .config/nvim/lua/crgwilson/lsp/dap.lua
@@ -26,6 +26,44 @@ dap_ui.setup({
     collapsed = ">",
     expanded = "v",
     current_frame = "->"
+  },
+  layouts = {
+    {
+      elements = {
+        {
+          id = "scopes",
+          size = 0.25
+        },
+        {
+          id = "breakpoints",
+          size = 0.25
+        },
+        {
+          id = "stacks",
+          size = 0.25
+        },
+        {
+          id = "watches",
+          size = 0.25
+        }
+      },
+      position = "left",
+      size = 40
+    },
+    {
+      elements = {
+        {
+          id = "console",
+          size = 0.75
+        },
+        {
+          id = "repl",
+          size = 0.25
+        }
+      },
+      position = "bottom",
+      size = 10
+    }
   }
 })
 dap_virtual_text.setup({
