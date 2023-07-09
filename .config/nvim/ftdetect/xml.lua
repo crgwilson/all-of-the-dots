bc523a70ffa3567eafa65e774bb0929b804d4ac1
Craig Wilson <craigjwilson93@gmail.com>
2023-07-08 22:20:10 -0700

    Decided to do the cfg -> xml thing for work another way

--- .config/nvim/ftdetect/xml.lua
+++
@@ -1,8 +0,0 @@
-local xml_augroup = vim.api.nvim_create_augroup("xml_augroup", { clear = true })
-vim.api.nvim_create_autocmd(
-  {"BufRead", "BufNewFile"}, {
-    pattern = {"*.xml", "*.cfg", "*.src"},
-    command = "set ft=xml",
-    group = xml_augroup
-  }
-)
