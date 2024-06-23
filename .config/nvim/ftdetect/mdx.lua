de7247318aff03e95b3bcbe918c5af906e384d45
Craig Wilson <craigjwilson93@gmail.com>
2024-06-23 11:31:56 -0700

    whoops, removing unintended commands from mdx

--- .config/nvim/ftdetect/mdx.lua
+++ .config/nvim/ftdetect/mdx.lua
@@ -1,6 +1,6 @@
 local mdx_augroup = vim.api.nvim_create_augroup("mdx_augroup", { clear = true })
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     pattern = { "*.mdx" },
-    command = "setlocal ft=markdown nobackup noswapfile",
+    command = "setlocal ft=markdown",
     group = mdx_augroup,
 })
